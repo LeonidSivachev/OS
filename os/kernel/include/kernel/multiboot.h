20a8fa8b2d1f04e6d821375f1fed39ede2b41a60
LeoS2005 <leonidsivachev@gmail.com>
2025-03-15 04:11:57 +0300

    continue_realization_multiboot2_tags_parsing

--- os/kernel/include/kernel/multiboot.h
+++ os/kernel/include/kernel/multiboot.h
@@ -94,4 +94,144 @@ struct multiboot_tag_bootdev {
     uint32_t part;        // Номер вложенного раздела (sub-partition)
 };
 
+struct multiboot_mmap_entry {
+    uint64_t addr;    // Начальный физический адрес региона
+    uint64_t len;     // Длина региона в байтах
+    uint32_t type;    // Тип региона (см. ниже)
+    uint32_t zero;    // Зарезервировано, всегда 0
+};
+
+struct multiboot_tag_mmap {
+    uint32_t type;      // Тип тега, равен 6 (MULTIBOOT_TAG_TYPE_MMAP)
+    uint32_t size;      // Общий размер тега в байтах
+    uint32_t entry_size;// Размер одной записи в таблице карты памяти
+    uint32_t entry_version; // Версия формата записей (обычно 0)
+    struct multiboot_mmap_entry entries[0]; // Массив записей карты памяти
+};
+
+struct vbe_info_block {
+    char signature[4];           // "VESA"
+    uint16_t version;  // Версия VBE (например, 0x0200 для 2.0)
+    uint32_t oem_string_ptr;  // Указатель на строку OEM (FAR-поинтер)
+    uint32_t capabilities;    // Возможности контроллера
+    uint32_t video_mode_ptr;  // Указатель на список поддерживаемых режимов
+    uint16_t total_memory;    // Общий объём видеопамяти в 64-КБ блоках
+    // Дополнительные поля для VBE 3.0 и выше
+    uint8_t reserved[492];
+};
+
+struct vbe_mode_info_block {
+    uint16_t mode_attributes; // Атрибуты режима (например, доступен ли LFB)
+    uint8_t win_a_attributes; // Атрибуты окна A
+    uint8_t win_b_attributes; // Атрибуты окна B
+    uint16_t win_granularity; // Гранулярность окна (КБ)
+    uint16_t win_size;        // Размер окна (КБ)
+    uint16_t win_a_segment;   // Сегмент окна A
+    uint16_t win_b_segment;   // Сегмент окна B
+    uint32_t win_func_ptr;    // Указатель на функцию переключения окон
+    uint16_t pitch;           // Байт на строку (байтов на линию)
+    uint16_t x_res;           // Разрешение по X (ширина)
+    uint16_t y_res;           // Разрешение по Y (высота)
+    uint8_t x_char_size;      // Ширина символа в текстовом режиме
+    uint8_t y_char_size;      // Высота символа в текстовом режиме
+    uint8_t number_of_planes; // Количество плоскостей
+    uint8_t bits_per_pixel;   // Бит на пиксель (глубина цвета)
+    uint8_t number_of_banks;  // Количество банков
+    uint8_t memory_model;     // Модель памяти (например, 4 = packed pixel)
+    uint8_t bank_size;        // Размер банка (КБ)
+    uint8_t number_of_image_pages; // Количество страниц изображения
+    uint8_t reserved1;
+    uint8_t red_mask_size;    // Размер маски красного цвета
+    uint8_t red_field_position; // Позиция красного цвета
+    uint8_t green_mask_size;  // Размер маски зелёного цвета
+    uint8_t green_field_position; // Позиция зелёного цвета
+    uint8_t blue_mask_size;   // Размер маски синего цвета
+    uint8_t blue_field_position; // Позиция синего цвета
+    uint8_t reserved_mask_size; // Размер резервной маски
+    uint8_t reserved_field_position; // Позиция резервной маски
+    uint8_t direct_color_mode_info; // Информация о прямом цвете
+    uint32_t phys_base_ptr;   // Физический адрес линейного фреймбуфера (LFB)
+    uint32_t reserved2[6];
+};
+
+struct multiboot_tag_vbe {
+    uint32_t type;            // Тип тега, равен 7 (MULTIBOOT_TAG_TYPE_VBE)
+    uint32_t size;            // Общий размер тега в байтах
+    uint16_t vbe_mode;        // Номер текущего VBE-режима
+    uint16_t vbe_interface_seg; // Сегмент интерфейса VBE (устаревшее)
+    uint16_t vbe_interface_off; // Смещение интерфейса VBE (устаревшее)
+    uint16_t vbe_interface_len; // Длина интерфейса VBE (устаревшее)
+    uint8_t vbe_control_info[512]; // Блок VBE Controller Information (VBE 2.0+)
+    uint8_t vbe_mode_info[256];    // Блок VBE Mode Information для текущего режима
+};
+
+struct multiboot_tag_framebuffer {
+    uint32_t type;          // Тип тега, равен 8 (MULTIBOOT_TAG_TYPE_FRAMEBUFFER)
+    uint32_t size;          // Общий размер тега в байтах
+    uint64_t framebuffer_addr; // Физический адрес фреймбуфера
+    uint32_t framebuffer_pitch; // Количество байт на строку
+    uint32_t framebuffer_width; // Ширина экрана в пикселях (или символах)
+    uint32_t framebuffer_height; // Высота экрана в пикселях (или символах)
+    uint8_t framebuffer_bpp;    // Бит на пиксель (глубина цвета)
+    uint8_t framebuffer_type;   // Тип фреймбуфера (см. ниже)
+    uint8_t reserved;           // Зарезервировано, обычно 0
+    union {
+        struct {
+            uint8_t framebuffer_red_field_position;   // Позиция красного цвета
+            uint8_t framebuffer_red_mask_size;        // Размер маски красного
+            uint8_t framebuffer_green_field_position; // Позиция зелёного цвета
+            uint8_t framebuffer_green_mask_size;      // Размер маски зелёного
+            uint8_t framebuffer_blue_field_position;  // Позиция синего цвета
+            uint8_t framebuffer_blue_mask_size;       // Размер маски синего
+        } framebuffer_rgb; // Для RGB-режима
+        struct {
+            uint16_t framebuffer_palette_num_colors; // Количество цветов в палитре
+            struct multiboot_color *framebuffer_palette;       // Указатель на палитру
+        } framebuffer_palette; // Для палитрового режима (устаревшее)
+    } u;
+};
+
+struct multiboot_tag_elf_sections {
+    uint32_t type;       // Тип тега, равен 9 (MULTIBOOT_TAG_TYPE_ELF_SECTIONS)
+    uint32_t size;       // Общий размер тега в байтах
+    uint32_t num;        // Количество секций в таблице
+    uint32_t entsize;    // Размер одной записи о секции (обычно 40 или 64 байта)
+    uint32_t shndx;      // Индекс секции строк (string table), содержащей имена секций
+    uint8_t sections[0]; // Массив записей о секциях (переменной длины)
+};
+
+struct multiboot_tag_apm {
+    uint32_t type;    // Тип тега, равен 10 (MULTIBOOT_TAG_TYPE_APM)
+    uint32_t size;    // Общий размер тега в байтах (обычно 20)
+    uint16_t version; // Версия APM (например, 0x0101 для 1.1)
+    uint16_t cseg;    // 16-битный сегмент кода APM (реальный режим)
+    uint32_t offset;  // 32-битное смещение точки входа APM
+    uint16_t cseg_16; // 16-битный сегмент кода для 16-битного защищённого режима
+    uint16_t dseg;    // 16-битный сегмент данных
+    uint16_t flags;   // Флаги состояния APM
+    uint16_t cseg_len;// Длина сегмента кода (в байтах)
+    uint16_t cseg_16_len; // Длина 16-битного сегмента кода
+    uint16_t dseg_len;// Длина сегмента данных
+};
+
+struct multiboot_tag_efi32 {
+    uint32_t type;       // Тип тега, равен 11 (MULTIBOOT_TAG_TYPE_EFI32)
+    uint32_t size;       // Общий размер тега в байтах (обычно 12)
+    uint32_t pointer;    // Указатель на EFI System Table (32-битный адрес)
+};
+
+struct multiboot_tag_efi64 {
+    uint32_t type;       // Тип тега, равен 12 (MULTIBOOT_TAG_TYPE_EFI64)
+    uint32_t size;       // Общий размер тега в байтах (обычно 16)
+    uint64_t pointer;    // Указатель на EFI System Table (64-битный адрес)
+};
+
+struct multiboot_tag_smbios {
+    uint32_t type;       // Тип тега, равен 13 (MULTIBOOT_TAG_TYPE_SMBIOS)
+    uint32_t size;       // Общий размер тега в байтах
+    uint8_t major;       // Мажорная версия SMBIOS
+    uint8_t minor;       // Минорная версия SMBIOS
+    uint8_t reserved[6]; // Зарезервировано (заполняется нулями)
+    uint8_t tables[0];   // Начало данных таблиц SMBIOS (переменной длины)
+};
 #endif
--- os/kernel/kernel/kernel.c
+++ os/kernel/kernel/kernel.c
@@ -7,7 +7,7 @@
 void parse_multiboot2_tags(uint32_t* multiboot_info_addr) {
     //printf("%s%d\n","size_of_multiboot_info_adr: ", *multiboot_info_addr);
     uint8_t* tag_ptr = (uint8_t*) multiboot_info_addr + 8;
-
+    printf("--------------------------------------------------------------------------------");
     while (((struct multiboot_tag*) tag_ptr)->type != MULTIBOOT_TAG_TYPE_END) {
         //printf("%s%d\n", "tag_type: ", ((struct multiboot_tag*) tag_ptr)->type );
         switch (((struct multiboot_tag*) tag_ptr)->type) {
@@ -15,18 +15,21 @@ void parse_multiboot2_tags(uint32_t* multiboot_info_addr) {
             {
                 printf("%d) ", MULTIBOOT_TAG_TYPE_CMDLINE);
                 printf("MULTIBOOT_TAG_TYPE_CMDLINE: %s\n", ((struct multiboot_tag_with_data*) tag_ptr)->data);
+                printf("--------------------------------------------------------------------------------");
                 break;
             }
             case MULTIBOOT_TAG_TYPE_BOOT_LOADER_NAME:
             {
                 printf("%d) ", MULTIBOOT_TAG_TYPE_BOOT_LOADER_NAME);
                 printf("MULTIBOOT_TAG_TYPE_BOOT_LOADER_NAME: %s\n", ((struct multiboot_tag_with_data*) tag_ptr)->data);
+                printf("--------------------------------------------------------------------------------");
                 break;
             }
             case MULTIBOOT_TAG_TYPE_MODULE:
             {
                 printf("%d) ", MULTIBOOT_TAG_TYPE_MODULE);
                 printf("MULTIBOOT_TAG_TYPE_MODULE: %s\n", ((struct multiboot_tag_with_data*) tag_ptr)->data);
+                printf("--------------------------------------------------------------------------------");
                 break;
             }
             case MULTIBOOT_TAG_TYPE_BASIC_MEMINFO: 
@@ -34,6 +37,7 @@ void parse_multiboot2_tags(uint32_t* multiboot_info_addr) {
                 printf("%d) ", MULTIBOOT_TAG_TYPE_BASIC_MEMINFO);
                 struct multiboot_tag_basic_meminfo* basic_meminfo_ptr = ((struct multiboot_tag_basic_meminfo*) tag_ptr);
                 printf("MULTIBOOT_TAG_TYPE_BASIC_MEMINFO: %d %d\n", basic_meminfo_ptr->mem_lower, basic_meminfo_ptr->mem_upper);
+                printf("--------------------------------------------------------------------------------");
                 break;
             }
             case MULTIBOOT_TAG_TYPE_BOOTDEV: // последние два некорректны
@@ -41,6 +45,79 @@ void parse_multiboot2_tags(uint32_t* multiboot_info_addr) {
                 printf("%d) ", MULTIBOOT_TAG_TYPE_BOOTDEV);
                 struct multiboot_tag_bootdev* type_bootdev_ptr = ((struct multiboot_tag_bootdev*) tag_ptr);
                 printf("MULTIBOOT_TAG_TYPE_BOOTDEV: %d %d %d\n",type_bootdev_ptr->biosdev, type_bootdev_ptr->slice, type_bootdev_ptr->part);
+                printf("--------------------------------------------------------------------------------");
+                break;
+            }
+            case MULTIBOOT_TAG_TYPE_MMAP:
+            {
+                printf("%d) ", MULTIBOOT_TAG_TYPE_MMAP);
+                struct multiboot_tag_mmap* mmap_tag_ptr = ((struct multiboot_tag_mmap*) tag_ptr);
+                printf("MULTIBOOT_TAG_TYPE_MMAP: %d, %d\n", mmap_tag_ptr->entry_size, mmap_tag_ptr->entry_version);
+                printf("entries:\n");
+                printf("addr: %d, len %d, type %d\n", mmap_tag_ptr->entries->addr, mmap_tag_ptr->entries->len, mmap_tag_ptr->entries->type);
+                printf("--------------------------------------------------------------------------------");
+                break;
+            }
+            case MULTIBOOT_TAG_TYPE_VBE: // не отображается
+            {
+                printf("%d) ", MULTIBOOT_TAG_TYPE_VBE);
+                struct multiboot_tag_vbe* vbe_tag_ptr = ((struct multiboot_tag_vbe*) tag_ptr);
+                printf("MULTIBOOT_TAG_TYPE_VBE\n");
+                printf("vbe_mode: %d", vbe_tag_ptr->vbe_mode);
+                printf("--------------------------------------------------------------------------------");
+                break;
+            }
+            case MULTIBOOT_TAG_TYPE_FRAMEBUFFER:
+            {
+                printf("%d) ", MULTIBOOT_TAG_TYPE_FRAMEBUFFER);
+                struct multiboot_tag_framebuffer* framebuffer_tag_ptr = ((struct multiboot_tag_frambuffer*) tag_ptr);
+                printf("MULTIBOOT_TAG_TYPE_FRAMEBUFFER\n");
+                printf("framebuffer_addr: %d\n", framebuffer_tag_ptr->framebuffer_addr);
+                printf("framebuffer_type: %d\n", framebuffer_tag_ptr->framebuffer_type);
+                printf("--------------------------------------------------------------------------------");
+                break;
+            }
+            case MULTIBOOT_TAG_TYPE_ELF_SECTIONS:
+            {
+                printf("%d) ", MULTIBOOT_TAG_TYPE_ELF_SECTIONS);
+                struct multiboot_tag_elf_sections* framebuffer_tag_ptr = ((struct multiboot_tag_elf_sections*) tag_ptr);
+                printf("MULTIBOOT_TAG_TYPE_ELF_SECTIONS\n");
+                printf("--------------------------------------------------------------------------------");
+                break;
+            }
+            case MULTIBOOT_TAG_TYPE_APM:
+            {
+                printf("%d) ", MULTIBOOT_TAG_TYPE_APM);
+                struct multiboot_tag_apm* apm_tag_ptr = ((struct multiboot_tag_apm*) tag_ptr);
+                printf("MULTIBOOT_TAG_TYPE_APM\n");
+                printf("apm_version: %d\n", apm_tag_ptr->version);
+                printf("--------------------------------------------------------------------------------");
+                break;
+            }
+            case MULTIBOOT_TAG_TYPE_EFI32: // не отображается 
+            {
+                printf("%d) ", MULTIBOOT_TAG_TYPE_EFI32);
+                struct multiboot_tag_efi32* efi32_tag_ptr = ((struct multiboot_efi32*) tag_ptr);
+                printf("MULTIBOOT_TAG_TYPE_EFI32\n");
+                printf("efi32_structure_pointer: %d\n", efi32_tag_ptr->pointer);
+                printf("--------------------------------------------------------------------------------");
+                break;
+            }
+            case MULTIBOOT_TAG_TYPE_EFI64: // не отображается 
+            {
+                printf("%d) ", MULTIBOOT_TAG_TYPE_EFI64);
+                struct multiboot_tag_efi64* efi64_tag_ptr = ((struct multiboot_efi64*) tag_ptr);
+                printf("MULTIBOOT_TAG_TYPE_EFI64\n");
+                printf("efi32_structure_pointer: %d\n", efi64_tag_ptr->pointer);
+                printf("--------------------------------------------------------------------------------");
+                break;
+            }
+            case MULTIBOOT_TAG_TYPE_SMBIOS: // не отображается
+            {
+                printf("%d) ", MULTIBOOT_TAG_TYPE_SMBIOS);
+                struct multiboot_tag_smbios* smbios_tag_ptr = ((struct multiboot_smbios*) tag_ptr);
+                printf("MULTIBOOT_TAG_TYPE_SMBIOS\n");
+                printf("--------------------------------------------------------------------------------");
                 break;
             }
         }
